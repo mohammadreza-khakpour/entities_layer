435fee9fd2160a9ddd36a064981277c63aded55c
khakpour <rezakhakpoorboraz@gmail.com>
2021-05-27 12:43:27 +0430

    second commit

--- Attendance.Entities/Attendance.cs
+++ Attendance.Entities/Attendance.cs
@@ -5,7 +5,7 @@ namespace Attendance.Entities
     public class Attendance
     {
         public long Attendance_Id { get; set; }
-        public DateTime AttendTime { get; set; }
+        public DateTime Attendance_AttendTime { get; set; }
         public Guid EmployeeId { get; set; }
         public Employee Employee { get; set; }
     }
